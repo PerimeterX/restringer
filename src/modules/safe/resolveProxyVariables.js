3d225317ed74134b2ce0696c062f6278d19d154f
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-06 13:29:01 +0300

    Improve resolveProxyVariables:   - Replacing all variables, not just callees.   - Verify references are not modified, only referenced.   - Remove proxy assignment if it is no longer referenced in the following iteration.

--- src/modules/safe/resolveProxyVariables.js
+++ src/modules/safe/resolveProxyVariables.js
@@ -1,22 +1,23 @@
+const areReferencesModified = require(__dirname + '/../utils/areReferencesModified');
+
 /**
  * Replace proxied variables with their intended target.
  * E.g.
- * const a2b = atob;
- * console.log(a2b('NDI=')); // will be replaced with `console.log(atob('NDI='));`
+ * const a2b = atob;         // This line will be removed
+ * console.log(a2b('NDI=')); // This will be replaced with `console.log(atob('NDI='));`
  * @param {Arborist} arb
  * @return {Arborist}
  */
 function resolveProxyVariables(arb) {
 	const candidates = [...new Set(arb.ast.filter(n =>
-		n.type === 'Identifier' &&
-		n.parentKey === 'callee' &&
-		n.declNode?.parentNode?.type === 'VariableDeclarator' &&
-		n.declNode.parentKey === 'id' &&
-		n.declNode.parentNode?.init?.type === 'Identifier')
-		.map(n => n.declNode.parentNode))];
+		n.type === 'VariableDeclarator' &&
+		n?.init?.type === 'Identifier'))];
 	for (const c of candidates) {
-		const refs = c.id.references;
-		if (!refs || refs.parentKey === 'left') continue;
+		const refs = c.id.references || [];
+		// Remove proxy assignments if there are no further proxies assigned
+		if (!refs.find(n => ['right', 'init'].includes(n.parentKey) &&
+			['AssignmentExpression', 'VariableDeclarator'].includes(n.parentNode.type))) arb.markNode(c);
+		if (areReferencesModified(arb.ast, refs)) continue;
 		for (const ref of refs) {
 			arb.markNode(ref, c.init);
 		}
