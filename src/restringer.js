8bc43b445ccab246de253e2b2c800186ffd34643
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-16 13:59:25 +0300

    Rearrange deob methods for better results - unwrapping and resolving expressions before resolving calls

--- src/restringer.js
+++ src/restringer.js
@@ -87,23 +87,23 @@ class REstringer {
 	_safeDeobfuscationMethods() {
 		return [
 			normalizeEmptyStatements,
-			resolveProxyCalls,
 			consolidateNestedBlockStatements,
 			resolveRedundantLogicalExpressions,
+			resolveProxyCalls,
+			resolveProxyVariables,
+			resolveProxyReferences,
 			resolveMemberExpressionReferencesToArrayIndex,
 			resolveMemberExpressionsWithDirectAssignment,
 			parseTemplateLiteralsIntoStringLiterals,
 			resolveDeterministicIfStatements,
-			unwrapFunctionShells,
-			replaceFunctionShellsWithWrappedValue,
-			replaceFunctionShellsWithWrappedValueIIFE,
 			replaceCallExpressionsWithUnwrappedIdentifier,
 			replaceEvalCallsWithLiteralContent,
 			replaceIdentifierWithFixedAssignedValue,
 			replaceIdentifierWithFixedValueNotAssignedAtDeclaration,
 			resolveFunctionConstructorCalls,
-			resolveProxyVariables,
-			resolveProxyReferences,
+			replaceFunctionShellsWithWrappedValue,
+			replaceFunctionShellsWithWrappedValueIIFE,
+			unwrapFunctionShells,
 		];
 	}
 
@@ -117,10 +117,10 @@ class REstringer {
 			resolveAugmentedFunctionWrappedArrayReplacements,
 			resolveMemberExpressionsLocalReferences,
 			resolveDefiniteMemberExpressions,
-			resolveLocalCalls,
 			resolveBuiltinCalls,
 			resolveDeterministicConditionalExpressions,
 			resolveInjectedPrototypeMethodCalls,
+			resolveLocalCalls,
 			resolveEvalCallsOnNonLiterals,
 		];
 	}
