45c96716b3e4dc44307c450cd4b29b3acead7b47
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-18 23:50:37 +0300

    Rearrange tests by solve time

--- tests/obfuscated-samples.js
+++ tests/obfuscated-samples.js
@@ -4,9 +4,9 @@ module.exports = {
 	'Ant & Cockroach': 'ant.js',
 	'_$_': 'udu.js',
 	'Prototype Calls': 'prototypeCalls.js',
+	'Obfuscator.io': 'obfuscatorIo.js',
 	'Caesar+': 'caesar.js',
 	'eval(Ox$': 'evalOxd.js',
 	'$s': 'ds.js',
-	'Obfuscator.io': 'obfuscatorIo.js',
 	'Local Proxies': 'localProxies.js',
 };
