2a80a6f6066b0e9524ca7e48daac3b873a90a4f9
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-18 23:28:39 +0300

    Join conditions with logical OR and move heavy conditions to the end to avoid redundant iterations

--- src/modules/utils/areReferencesModified.js
+++ src/modules/utils/areReferencesModified.js
@@ -7,24 +7,24 @@ const {propertiesThatModifyContent} = require(__dirname + '/../config');
  */
 function areReferencesModified(ast, refs) {
 	// Verify no reference is on the left side of an assignment
-	return Boolean(refs.find(r => r.parentKey === 'left' &&
-			['AssignmentExpression', 'ForInStatement', 'ForOfStatement'].includes(r.parentNode.type)) ||
+	return Boolean(refs.find(r =>
+		(r.parentKey === 'left' && ['AssignmentExpression', 'ForInStatement', 'ForOfStatement'].includes(r.parentNode.type)) ||
 		// Verify no reference is part of an update expression
-		refs.find(r => r.parentNode.type === 'UpdateExpression') ||
+		r.parentNode.type === 'UpdateExpression' ||
 		// Verify no variable with the same name is declared in a subscope
-		refs.find(r => r.parentNode.type === 'VariableDeclarator' && r.parentKey === 'id') ||
+		(r.parentNode.type === 'VariableDeclarator' && r.parentKey === 'id') ||
+		// Verify no modifying calls are executed on any of the references
+		(r.parentNode.type === 'MemberExpression' &&
+			r.parentNode.parentNode.type === 'CallExpression' &&
+			r.parentNode.parentNode.callee?.object === r &&
+			propertiesThatModifyContent.includes(r.parentNode.property?.value || r.parentNode.property?.name)) ||
 		// Verify there are no member expressions among the references which are being assigned to
-		refs.find(r => r.type === 'MemberExpression' &&
+		(r.type === 'MemberExpression' &&
 			ast.find(n => n.type === 'AssignmentExpression' &&
 				n.left.type === 'MemberExpression' &&
 				n.left.object?.name === r.object?.name &&
 				(n.left.property?.name || n.left.property?.value === r.property?.name || r.property?.value) &&
-				(n.left.object.declNode?.nodeId && (r.object.declNode?.nodeId || r.object?.nodeId) === n.left.object.declNode.nodeId))) ||
-		// Verify no modifying calls are executed on any of the references
-		refs.find(r => r.parentNode.type === 'MemberExpression' &&
-			r.parentNode.parentNode.type === 'CallExpression' &&
-			r.parentNode.parentNode.callee?.object?.nodeId === r.nodeId &&
-			propertiesThatModifyContent.includes(r.parentNode.property?.value || r.parentNode.property?.name)));
+				(n.left.object.declNode && (r.object.declNode || r.object) === n.left.object.declNode)))));
 }
 
 module.exports = areReferencesModified;
