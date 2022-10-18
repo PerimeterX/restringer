d903b3b64da985ab7f30c797bfc5121ea7907e8f
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-18 23:48:19 +0300

    Remove unneeded edge case handling

--- src/modules/safe/resolveFunctionConstructorCalls.js
+++ src/modules/safe/resolveFunctionConstructorCalls.js
@@ -13,8 +13,6 @@ function resolveFunctionConstructorCalls(arb) {
 		(n.callee.property?.name || n.callee.property?.value) === 'constructor' &&
 		n.arguments.length && n.arguments.slice(-1)[0].type === 'Literal');
 	for (const c of candidates) {
-		// TODO: Without the next line we get an anonymous function. Is that bad?
-		if (!['VariableDeclarator', 'AssignmentExpression'].includes(c.parentNode.type)) continue;
 		let args = '';
 		if (c.arguments.length > 1) {
 			const originalArgs = c.arguments.slice(0, -1);
