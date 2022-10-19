b603541a5ecae5f3a2da78488a53324d471b9e91
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-19 10:24:25 +0300

    Change logger default level to NONE; Fix issue with NONE log level being the exact opposite; Fix wrong use of the 'in' operator;

--- src/modules/utils/logger.js
+++ src/modules/utils/logger.js
@@ -5,15 +5,15 @@
  * Debug mode can also be enabled from the outer scope when this module is being imported
  */
 const logLevels = {
-	NONE: -1,
 	DEBUG: 1,
 	LOG: 2,
 	ERROR: 3,
+	NONE: 9e10,
 };
-let currentLogLevel = logLevels.LOG;
+let currentLogLevel = logLevels.NONE;
 
 function createLoggerForLevel(logLevel) {
-	if (!(logLevel in Object.values(logLevels))) throw new Error(`Unknown log level ${logLevel}.`);
+	if (!Object.values(logLevels).includes(logLevel)) throw new Error(`Unknown log level ${logLevel}.`);
 	return msg => logLevel >= currentLogLevel ? console.log(msg) : undefined;
 }
 const debug = createLoggerForLevel(logLevels.DEBUG);
@@ -25,7 +25,7 @@ const error = createLoggerForLevel(logLevels.ERROR);
  * @param {number} newLogLevel
  */
 function setLogLevel(newLogLevel) {
-	if (!(newLogLevel in Object.values(logLevels))) throw new Error(`Unknown log level ${newLogLevel}.`);
+	if (!Object.values(logLevels).includes(newLogLevel)) throw new Error(`Unknown log level ${newLogLevel}.`);
 	currentLogLevel = newLogLevel;
 }
 
