724aa1de186f917e2074d6edd291e6916ca85e36
Ben Baryo <ben.baryo@perimeterx.com>
2022-10-23 20:53:14 +0300

    Add an optional limiter to the number of overall iterations

--- src/modules/utils/runLoop.js
+++ src/modules/utils/runLoop.js
@@ -1,9 +1,17 @@
 const {Arborist} = require('flast');
 const generateHash = require(__dirname + '/generateHash');
 const logger = require(__dirname + '/../utils/logger');
-const {defaultMaxIterations} = require(__dirname + '/../config');
+const {defaultMaxIterations, getGlobalMaxIterations} = require(__dirname + '/../config');
 
-let iterationsCounter = 0;
+let globalIterationsCounter = 0;
+
+/**
+ * @returns {boolean} false if iterations limiting is disabled (-1)
+ *                    or the global iterations counter hasn't reached its maximum allowed iterations; true otherwise.
+ */
+function hasGlobalMaxIterationBeenReached() {
+	return getGlobalMaxIterations() === -1 ? false : globalIterationsCounter >= getGlobalMaxIterations();
+}
 
 /**
  * Run functions which modify the script in a loop until they are no long effective or the maximum number of cycles is reached.
@@ -19,7 +27,7 @@ function runLoop(script, funcs, maxIterations = defaultMaxIterations) {
 		let scriptHash = generateHash(script);
 		let changesCounter = 0;
 		let arborist = new Arborist(script, logger.log);
-		while (arborist.ast.length && scriptSnapshot !== script && currentIteration < maxIterations) {
+		while (arborist.ast.length && scriptSnapshot !== script && currentIteration < maxIterations && !hasGlobalMaxIterationBeenReached()) {
 			const cycleStartTime = Date.now();
 			scriptSnapshot = script;
 			arborist.ast.forEach(n => n.scriptHash = scriptHash);   // Mark each node with the script hash to distinguish cache of different scripts.
@@ -40,20 +48,20 @@ function runLoop(script, funcs, maxIterations = defaultMaxIterations) {
 						arborist.ast.forEach(n => n.scriptHash = scriptHash);
 					}
 				} catch (e) {
-					logger.error(`[-] Error in ${func.name} (iteration #${iterationsCounter}): ${e}\n${e.stack}`);
+					logger.error(`[-] Error in ${func.name} (iteration #${globalIterationsCounter}): ${e}\n${e.stack}`);
 				} finally {
 					logger.debug(`\t\t[!] Running ${func.name} completed in ` +
 						`${((+new Date() - funcStartTime) / 1000).toFixed(3)} seconds`);
 				}
 			}
 			++currentIteration;
-			++iterationsCounter;
-			logger.log(`[+] ==> Cycle ${iterationsCounter} completed in ${(Date.now() - cycleStartTime) / 1000} seconds` +
+			++globalIterationsCounter;
+			logger.log(`[+] ==> Cycle ${globalIterationsCounter} completed in ${(Date.now() - cycleStartTime) / 1000} seconds` +
 				` with ${changesCounter ? changesCounter : 'no'} changes (${arborist.ast.length} nodes)`);
 		}
 		if (changesCounter) script = arborist.script;
 	} catch (e) {
-		logger.error(`[-] Error on loop #${iterationsCounter}: ${e}\n${e.stack}`);
+		logger.error(`[-] Error on loop #${globalIterationsCounter}: ${e}\n${e.stack}`);
 	}
 	return script;
 }
