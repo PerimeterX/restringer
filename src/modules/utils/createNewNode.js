e8527062cf6c9079bb94d7e5b2276cc203391d41
Ben Baryo <19845603+ctrl-escp@users.noreply.github.com>
2022-12-02 18:44:04 +0200

    Handle edge case where a string is prefixed by a '-' but is not a number

--- src/modules/utils/createNewNode.js
+++ src/modules/utils/createNewNode.js
@@ -17,7 +17,14 @@ function createNewNode(value) {
 			case 'Number':
 			case 'Boolean':
 				if (['-', '+', '!'].includes(String(value)[0]) && String(value).length > 1) {
-					newNode = {
+					const absVal = String(value).substring(1);
+					if (Number.isNaN(parseInt(absVal)) && !['Infinity', 'NaN'].includes(absVal)) {
+						newNode = {
+							type: 'Literal',
+							value,
+							raw: String(value),
+						};
+					} else newNode = {
 						type: 'UnaryExpression',
 						operator: String(value)[0],
 						argument: createNewNode(String(value).substring(1)),
