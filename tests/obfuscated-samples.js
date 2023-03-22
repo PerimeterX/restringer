7ab236e40733c14c52418d7767b5df77dbdee6c8
Ben Baryo <ben.baryo@perimeterx.com>
2023-03-22 18:52:05 +0200

    Add obfuscated sample of hunter.js that uses eval on a function with context outside of eval

--- tests/obfuscated-samples.js
+++ tests/obfuscated-samples.js
@@ -2,6 +2,7 @@ module.exports = {
 	// Order by approximate deobfuscation time, ascending
 	'JSFuck': 'jsfuck.js',
 	'Ant & Cockroach': 'ant.js',
+	'Hunter': 'hunter.js',
 	'_$_': 'udu.js',
 	'Prototype Calls': 'prototypeCalls.js',
 	'Obfuscator.io': 'obfuscatorIo.js',
