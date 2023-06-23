ca168be3668058ce61f126bca8f2428bcb49989d
Ben Baryo <ben.baryo@perimeterx.com>
2023-06-23 14:10:43 +0300

    Improve performance by removing redundant iterations. Replaced the more expensive generateFlatAST with its underlining parseCode

--- src/modules/utils/createOrderedSrc.js
+++ src/modules/utils/createOrderedSrc.js
@@ -1,4 +1,4 @@
-const {generateFlatAST} = require('flast');
+const {parseCode} = require('flast');
 
 const largeNumber = 999e8;
 const sortByNodeId = (a, b) => a.nodeId > b.nodeId ? 1 : b.nodeId > a.nodeId ? -1 : 0;
@@ -10,8 +10,9 @@ const sortByNodeId = (a, b) => a.nodeId > b.nodeId ? 1 : b.nodeId > a.nodeId ? -
  * @return {string} Combined source code of the nodes.
  */
 function createOrderedSrc(nodes, preserveOrder = false) {
+	const parsedNodes = [];
 	for (let i = 0; i < nodes.length; i++)  {
-		const n = nodes[i];
+		let n = nodes[i];
 		if (n.type === 'CallExpression') {
 			if (n.parentNode.type === 'ExpressionStatement') {
 				nodes[i] = n.parentNode;
@@ -25,9 +26,11 @@ function createOrderedSrc(nodes, preserveOrder = false) {
 					const altFuncName = (n.parentNode.type === 'VariableDeclarator' ? n.parentNode.id.name : 'func' + n.nodeId);
 					const funcStartRegexp = new RegExp('function[^(]*');
 					const funcSrc = n.callee?.id ? n.src : n.src.replace(funcStartRegexp, 'function ' + altFuncName);
-					const newNode = generateFlatAST(`(${funcSrc});`)[1];
+					const src = `(${funcSrc});`;
+					const newNode = parseCode(src);
 					if (newNode) {
 						newNode.nodeId = n.nodeId + largeNumber;
+						newNode.src = src;
 						nodes[i] = newNode;
 					}
 				} else nodes[i] = n;
@@ -36,18 +39,22 @@ function createOrderedSrc(nodes, preserveOrder = false) {
 			if (n.parentNode.type === 'VariableDeclarator') {
 				const funcStartRegexp = new RegExp('function[^(]*');
 				const funcSrc = n.src.replace(funcStartRegexp, 'function ' + n.parentNode.id.name);
-				const parsedNode = generateFlatAST(`(${funcSrc});`)[1];
+				const src = `(${funcSrc});`;
+				const parsedNode = parseCode(src);
 				if (parsedNode) {
 					parsedNode.nodeId = n.nodeId;
+					parsedNode.src = src;
 					nodes[i] = parsedNode;
 				}
 			}
 		}
+		n = nodes[i];	// In case the node was replaced
+		if (!parsedNodes.includes(n)) parsedNodes.push(n);
 	}
-	const orderedNodes = [...new Set(nodes)].sort(sortByNodeId);
+	parsedNodes.sort(sortByNodeId);
 	let output = '';
-	for (let i = 0; i < orderedNodes.length; i++)  {
-		const n = orderedNodes[i];
+	for (let i = 0; i < parsedNodes.length; i++)  {
+		const n = parsedNodes[i];
 		const addSemicolon = ['VariableDeclarator', 'AssignmentExpression'].includes(n.type);
 		output += (n.type === 'VariableDeclarator' ? `${n.parentNode.kind} ` : '') + n.src + (addSemicolon ? ';' : '') + '\n';
 	}
