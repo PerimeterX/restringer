54521fc334ed859a47aa059b74c5e2e1ea97e266
Ben Baryo <ben.baryo@perimeterx.com>
2023-06-23 17:19:10 +0300

    Replace generateFlatAST with parseCode for faster results

--- src/utils/cleanScript.js
+++ src/utils/cleanScript.js
@@ -2,9 +2,9 @@
  * Pass scripts through this tool to help compare pre- and post- deobfuscation
  */
 const fs = require('node:fs');
-const {generateFlatAST, generateCode} = require('flast');
+const {parseCode, generateCode} = require('flast');
 
 const inFileName = process.argv[2];
 const outFileName = inFileName + '-clean.js';
-fs.writeFileSync(outFileName, generateCode(generateFlatAST(fs.readFileSync(inFileName, 'utf-8'))[0]));
+fs.writeFileSync(outFileName, generateCode(parseCode(fs.readFileSync(inFileName, 'utf-8'))));
 console.log(`[+] Created clean file - ${outFileName}`);
