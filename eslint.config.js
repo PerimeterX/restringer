fcefd3d5dad9889b36a251db98e71ab58ffb5d8f
Ben Baryo <ben.baryo@humansecurity.com>
2024-10-11 16:38:39 +0300

    Update eslint config

---
+++ eslint.config.js
@@ -0,0 +1,46 @@
+import globals from 'globals';
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import js from '@eslint/js';
+import {FlatCompat} from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+	baseDirectory: __dirname,
+	recommendedConfig: js.configs.recommended,
+	allConfig: js.configs.all
+});
+
+export default [
+	{
+		ignores: [
+			'tests/resources',
+			'**/jquery*.js',
+			'**/*tmp*.*',
+			'**/*tmp*/'
+		],
+	},
+	...compat.extends('eslint:recommended'),
+	{
+		languageOptions: {
+			globals: {
+				...globals.browser,
+				...globals.node,
+				...globals.commonjs,
+			},
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+		},
+		rules: {
+			indent: ['error', 'tab', {
+				SwitchCase: 1,
+			}],
+			'linebreak-style': ['error', 'unix'],
+			quotes: ['error', 'single', {
+				allowTemplateLiterals: true,
+			}],
+			semi: ['error', 'always'],
+			'no-empty': ['off'],
+		},
+	}];
