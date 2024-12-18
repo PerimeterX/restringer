04f77eaa119a334e681977b70540026fa867ea84
Ben Baryo <ben.baryo@humansecurity.com>
2024-12-18 15:58:25 +0200

    Replace sha256 with md5

--- src/modules/utils/generateHash.js
+++ src/modules/utils/generateHash.js
@@ -1,7 +1,7 @@
 import crypto from 'node:crypto';
 
 function generateHash(script) {
-	return crypto.createHash('sha256').update(script).digest('hex');
+	return crypto.createHash('md5').update(script).digest('hex');
 }
 
 export {generateHash};
